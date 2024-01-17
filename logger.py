2809e2091a16b72d61516cf9956036ec8b250139
ahmed.sif.benmessaoud.13@gmail.com <benx13@MacBook-Pro.local>
2024-01-17 01:34:07 -0800

    logger class

---
+++ logger.py
@@ -0,0 +1,46 @@
+from utils import frame_to_hms
+
+
+class Logger():
+    def __init__(self):
+        self.stats = {
+                'total': {'grab': 0,
+                            'forward': 0,
+                            'backward':0,
+                            'machine':0,
+                            'transition':0,
+                            'events':[]
+                },
+                'per_bag':[] 
+        }
+        self.buffer = {'grab': 0,
+        'forward': 0,
+        'backward':0,
+        'machine':0,
+        'events': []
+        }
+        self.logs = []
+
+    def reset_buffer(self):
+        self.buffer = {'grab': 0,
+            'forward': 0,
+            'backward':0,
+            'machine':0,
+            'events': []
+            }
+
+    def update(self, event, logging_time):
+        self.stats['total'][event] += 1  
+        self.stats['total'][event].append(f"{event[0]}{self.stats['total'][event]}") 
+        self.buffer['events'].append(f"{event[0]}{self.stats['total'][event]}") 
+        self.logs.append(
+            {'message':f'{event} happened at: {logging_time}',
+                'frames_left':250,
+                'total_frames':250
+                })
+        if event == 'transition':
+            self.stats['per_bag'].append(self.buffer)
+            self.buffer = self.reset_buffer()
+
+    def update_logs(self):
+        self.logs = list(filter(lambda log: log['frames_left'] > 0, self.logs))
